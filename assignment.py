68c8257c90a8fb23e1d814b044f4446127cd92e3
suman98 <41943773+suman98@users.noreply.github.com>
2024-08-05 08:32:31 +0545

    task completed

---
+++ assignment.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import Sequential
+from keras.layers import Embedding, LSTM, Dense, Dropout
+from keras.utils.np_utils import to_categorical
+
+print('done')
